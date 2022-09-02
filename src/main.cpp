7dff8faddbe746b6c5b2c3d3bbd8770b57c2fde7
DenisOgnev <denisognev@icloud.com>
2022-09-02 16:12:36 +0300

    basic tracing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <glm/glm.hpp>
+#include <limits>
+#include <vector>
+#include <optional>
 
 const std::string WINDOW_NAME = "Test";
 const int32_t WIDTH = 500;
 const int32_t HEIGHT = 500;
 const sf::Vector2u WINDOW_SIZE(WIDTH, HEIGHT);
+
+struct Sphere
+{
+    glm::vec3 center;
+    float radius;
+    sf::Color color;
+};
+
+const std::vector<Sphere> spheres{{glm::vec3(0.0f, -1.0f, 3.0f), 1.0f, sf::Color::Blue}};
+
 
 void put_pixel_start_from_zero(sf::Uint8* pixels, uint32_t x, uint32_t y, sf::Color color)
 {
@@ -45,6 +58,56 @@ void fill(sf::Uint8* pixels, sf::Color color)
     }
 }
 
+std::tuple<float, float> intersect_ray_sphere(glm::vec3 camera_pos, glm::vec3 direction, Sphere sphere)
+{
+    glm::vec3 C = sphere.center;
+    float r = sphere.radius;
+    glm::vec3 OC = camera_pos - C;
+
+    float k1 = glm::dot(direction, direction);
+    float k2 = 2 * glm::dot(OC, direction);
+    float k3 = glm::dot(OC, OC) - r * r;
+
+    float discriminant = k2 * k2 - 4 * k1 * k3;
+    if (discriminant < 0)
+    {
+        return std::make_tuple(std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity());
+    }
+
+    float t1 = (-k2 + std::sqrtf(discriminant)) / (2 * k1);
+    float t2 = (-k2 - std::sqrtf(discriminant)) / (2 * k1);
+
+    return std::make_tuple(t1, t2);
+}
+
+sf::Color trace_ray(glm::vec3 camera_pos, glm::vec3 direction, float t_min, float t_max)
+{
+    float closest_t = std::numeric_limits<float>::infinity();
+    std::optional<Sphere> closest_sphere;
+    for (const auto& sphere : spheres)
+    {
+        float t1, t2;
+        std::tie(t1, t2) = intersect_ray_sphere(camera_pos, direction, sphere);
+        if (t1 > t_min && t1 < t_max && t1 < closest_t)
+        {
+            closest_t = t1;
+            closest_sphere = sphere;
+        }
+        if (t2 > t_min && t2 < t_max && t2 < closest_t)
+        {
+            closest_t = t2;
+            closest_sphere = sphere;
+        }
+    }
+    if (!closest_sphere)
+    {
+        return sf::Color::White;
+    }
+
+    return closest_sphere.value().color;
+}
+
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE), WINDOW_NAME);
@@ -57,11 +120,27 @@ int main()
     }
     sf::Sprite sprite(texture);
 
-    glm::vec3 pos(1.0f, 1.0f, 1.0f);
-
     fill(pixels, sf::Color::Black);
     put_pixel(pixels, 249, 249, sf::Color::White);
 
+    texture.update(pixels);
+
+
+    glm::vec3 camera_pos(0.0f, 0.0f, 0.0f);
+    uint32_t viewport_width = 1;
+    uint32_t viewport_height = 1;
+    uint32_t plane_distance = 1;
+
+    for (int32_t x = -WIDTH / 2; x < (WIDTH + 1) / 2; x++)
+    {
+        for (int32_t y = -HEIGHT / 2; y < (HEIGHT + 1) / 2; y++)
+        {
+            glm::vec3 direction(x * (viewport_width / static_cast<double>(WIDTH)), y * (viewport_height / static_cast<double>(HEIGHT)), plane_distance);
+            sf::Color result_color = trace_ray(camera_pos, direction, 1.0f, std::numeric_limits<float>::infinity());
+            put_pixel(pixels, x, y, result_color);
+        }
+    }
+    
     texture.update(pixels);
     
 
